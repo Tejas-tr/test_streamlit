e163908ee407440b937ef3a5a1e643c8b9a154a1
Tejas-tr <155260687+Tejas-tr@users.noreply.github.com>
2025-01-13 00:11:51 +0530

    code change added few extre widgets

--- stockmarket.py
+++ stockmarket.py
@@ -7,20 +7,31 @@ st.write('this is hopt of getting hike !!')
 
 # import datetime
 import datetime
+col1, col2 = st.columns(2)
 
-startdate = st.date_input('Please enter Starting Date', datetime.date(2019,1,1))
+with col1:
+    st.header("Start date")
+    startdate = st.date_input('Please enter Starting Date', datetime.date(2019,1,1))
 
-enddate =  st.date_input('Please enter Ending Date', datetime.date(2024,12,31))
+with col2:
+    st.header("End date")
+    enddate =  st.date_input('Please enter Ending Date', datetime.date(2024,12,31))
 
 
-ticker_symbol = 'AAPL'
+#ticker_symbol = 'AAPL'
+
+ticker_symbol = st.text_input('Enter the Stock market ticker symbol', 'AAPL')
+
 ticker_data =  yf.Ticker(ticker_symbol)
 ticker_df = ticker_data.history(period = '1d', start = startdate, end = enddate)
 
+st.header("Data")
 st.dataframe(ticker_df)
 
-st.write('## Daily closing price')
-st.line_chart(ticker_df['Close'])
-
-st.write("## Volume movement over time.")
-st.line_chart(ticker_df['Volume'])
+col1, col2 = st.columns(2)
+with col1:
+    st.write('## Daily closing price')
+    st.line_chart(ticker_df['Close'])
+with col2:
+    st.write("## Volume movement over time.")
+    st.line_chart(ticker_df['Volume'])
